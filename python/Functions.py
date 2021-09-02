25e41bbe3bcab5c0ed94ad6f2560dda28796ba7b
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-02 22:44:00 +0530

    functions, input & output

---
+++ python/Functions.py
@@ -0,0 +1,26 @@
+def addNumbers():
+    print(5 + 10)
+
+def subNumbers(x, y):
+    print(x - y)
+
+def multiplyNumbers(x, y, z):
+    return x * y * z
+
+
+addNumbers()
+subNumbers(100, 200)
+p1 = multiplyNumbers(5, 10, 15)
+p2 = multiplyNumbers(10, 20, 30)
+
+print("The products are", p1, p2)
+
+'''
+Types of functions:
+- On the basis of parameters
+    1. Default 
+    2. Parameterised
+- On the basis of return values
+    1. Return values
+    2. No return value
+'''
---
+++ python/Hello.py
@@ -0,0 +1,15 @@
+'''
+print("Hello!")
+
+name = input("Enter your name: ")
+print("Hi " + name)
+'''
+
+first = int(input("Enter first number: "))
+second = int(input("Enter second number: "))
+
+print(first + second, end="\t")
+print(first - second, end="\t")
+print(first * second, end="\t")
+print(first / second, end="\t")
+print(first % second)
